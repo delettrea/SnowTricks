667b875cd4a3281a057499fc5be7ca7ec888e273
delettrea <delettrealine@gmail.com>
2018-02-26 17:18:47 +0100

    add tricksController and templates for tricks

---
+++ src/Controller/TricksController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Tricks;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class TricksController extends Controller
+{
+
+    /**
+     * @Route("/", name="home_page")
+     */
+    public function index()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $tricks = $em->getRepository('App:Tricks')->tricksIndex();
+
+        return $this->render('tricks/index.html.twig', [
+            'tricks' => $tricks,
+        ]);
+    }
+
+    /**
+     * @Route("/tricks/details/{id}", name="trick_details")
+     * @Method("GET")
+     */
+    public function details(Tricks $tricks)
+    {
+        return $this->render('tricks/details.html.twig', [
+            'tricks' => $tricks,
+        ]);
+    }
+
+}
