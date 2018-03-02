453450e2b557f9ea3b102871e3baff8de96d2bb8
delettrea <delettrealine@gmail.com>
2018-03-02 18:54:11 +0100

    add UserFixtures

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,40 @@
+<?php
+
+
+namespace App\DataFixtures;
+
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+
+class UserFixtures extends Fixture implements ContainerAwareInterface
+{
+    use  ContainerAwareTrait;
+
+    public function load(ObjectManager $manager)
+    {
+        $tab = array(
+            array('username' => 'User1' , 'email' => 'user1@test.test', 'password' =>'user' ),
+            array('username' => 'User2' , 'email' => 'user2@test.test', 'password' =>'user' ),
+            array('username' => 'User3' , 'email' => 'user3@test.test', 'password' =>'user' ),
+        );
+
+        $passwordEncoder = $this->container->get('security.password_encoder');
+
+        foreach ($tab as $row)
+        {
+            $user = new User();
+            $user->setUsername($row['username']);
+            $user->setEmail($row['email']);
+            $encodedPassword = $passwordEncoder->encodePassword($user, $row['password']);
+            $user->setPassword($encodedPassword);
+
+            $manager->persist($user);
+        }
+
+        $manager->flush();
+    }
+}
